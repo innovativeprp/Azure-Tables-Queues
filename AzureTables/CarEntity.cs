c0e5f70254ff442da8fe7a3d4d9bb2b7fc55f995
Praveen Raveendran <Praveen.Raveendran@hotmail.com>
2018-07-05 00:07:23 +0530

    AzureTables

---
+++ AzureTables/CarEntity.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureTables
+{
+    class CarEntity:TableEntity
+    {
+        public CarEntity()
+        {
+
+        }
+        public CarEntity(int id,string make,string model,string color)
+        {
+            Id = id;
+            Make = make;
+            Model = model;
+            Color = color;
+            PartitionKey = "car";
+            RowKey = Id.ToString();
+        }
+
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+    }
+}
--- AzureTables/Program.cs
+++ AzureTables/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Configuration;
+
 
 namespace AzureTables
 {
@@ -10,6 +10,46 @@ namespace AzureTables
     {
         static void Main(string[] args)
         {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings.Get("StorageConnectionString"));
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable cloudTable = tableClient.GetTableReference("FirstTable");
+            cloudTable.CreateIfNotExists();
+             
+            var carEntity = new CarEntity(3, "Honda", "Civic", "MSG");
+            // InsertCarData(cloudTable, carEntity);
+
+            //RetrieveCarData(cloudTable);
+            CarEntityQuery(cloudTable);
+
+            Console.WriteLine("Completed");
+            Console.ReadKey();
+        }
+
+        private static void InsertCarData(CloudTable table,CarEntity carEntity)
+        {
+            TableOperation operation = TableOperation.Insert(carEntity);
+            table.Execute(operation);
+        }
+        private static void RetrieveCarData(CloudTable table)
+        {
+            TableOperation operation = TableOperation.Retrieve<CarEntity>("car", "3");
+            TableResult result = table.Execute(operation);
+            
+            if(result.Result==null)
+            {
+                Console.WriteLine("Car not found!!!");
+            }
+            CarEntity retrievedCar = (CarEntity)(result.Result);
+            Console.WriteLine($"Car found with model {retrievedCar.Model} , make is {retrievedCar.Make} and color is {retrievedCar.Color}");
+        }
+
+        private static void CarEntityQuery(CloudTable table)
+        {
+            var query = new TableQuery<CarEntity>();
+          foreach(var item in table.ExecuteQuery(query))
+            {
+                Console.WriteLine($"Car make is {item.Make} and model is {item.Model}");
+            }
         }
     }
 }
